2eb35ca2b3940ccb5a442a61a95fa83d95684874
Aarnav Srinivasan <aarnav.srinivasan@gmail.com>
2022-07-23 23:02:56 +0800

    Add cuisine filtering to filterData function (broken - Invalid hook call)

--- src/Eat-at-NUS/Database/FilterData.js
+++ src/Eat-at-NUS/Database/FilterData.js
@@ -1,5 +1,4 @@
 // Importing the required stuff
-import React from "react";
 import { useState, useEffect } from "react";
 
 import { db } from "../../../firebase/Eat-at-NUS_config";
@@ -8,67 +7,136 @@ import { onValue, ref, query, orderByChild, equalTo } from "firebase/database";
 const filterData = (name, cuisines) => {
 
     const [stallIDs, setStallIDs] = useState(new Set());
-    const [dishresult, setDishResult] = useState([]);
+    const [dishResult, setDishResult] = useState([]);
     const [stallResult, setStallResult] = useState([]);
 
-    
+
     // Filtering by name
 
     // Finding stalls that serve the dish with the given name
-    const dishesReference = ref(db, 'dishes');
-    const stallIDsQuery = query(dishesReference, orderByChild('name'), equalTo(name));
+    useEffect(() => {
+        
+        if (name != '') {
+            const dishesReference = ref(db, 'dishes');
+            const stallIDsQuery = query(dishesReference, orderByChild('name'), equalTo(name));
+    
+            onValue(stallIDsQuery, (snapshot) => {
+                const stallIDsAnswer = new Set();
+    
+                snapshot.forEach((child) => {
+                    stallIDsAnswer.add(child.val().stall);
+                });
+    
+                setStallIDs(stallIDsAnswer);
+    
+            });
+        }
+
+    }, [db]);
 
     useEffect(() => {
-        onValue(stallIDsQuery, (snapshot) => {
-            const data = snapshot.val();
 
-            if (data) {
-                const stallIDsAnswer = new Set();
-                Object.values(data).forEach((obj) => {
-                    stallIDsAnswer.add(obj.stall);
-                });
-                setStallIDs(stallIDsAnswer);
-            }
+        if (stallIDs.size > 0) {
+            const stallsMetadataReference = ref(db, 'stallsMetadata');
 
-        });
-    }, []);
-
-    const stallsMetadataReference = ref(db, 'stallsMetadata');
-
-    if (stallIDs.size > 0) {
-        useEffect(() => {
             onValue(stallsMetadataReference, (snapshot) => {
-                const data = snapshot.val();
-                const stallsList = [];
-                Object.keys(data).forEach((key) => {
-                    if (stallIDs.has(key)) {
-                        stallsList.push(data[key]);
+                const stallsMetadataList = [];
+    
+                snapshot.forEach((child) => {
+                    if (stallIDs.has(child.key)){
+                        stallsMetadataList.push({
+                            id: child.key,
+                            name: child.val().name,
+                            cuisine: child.val().cuisine,
+                            rating: child.val().rating
+                        });
                     }
                 });
-                setDishResult(stallsList);
+    
+                setDishResult(stallsMetadataList);
+    
             });
-        }, []);
-    }
+        }
+
+    }, [db]);
 
 
     // Finding stalls with the given name
-    const stallNameQuery = query(stallsMetadataReference, orderByChild('name'), equalTo(name));
-
     useEffect(() => {
-        onValue(stallNameQuery, (snapshot) => {
-            const data = snapshot.val();
+        
+        if (name != '') {
+            const stallsMetadataReference = ref(db, 'stallsMetadata');
+            const stallNameQuery = query(stallsMetadataReference, orderByChild('name'), equalTo(name));
+    
+            onValue(stallNameQuery, (snapshot) => {
+                const stallResultAnswer = [];
+    
+                snapshot.forEach((child) => {
+                    stallResultAnswer.push({
+                        id: child.key,
+                        name: child.val().name,
+                        cuisine: child.val().cuisine,
+                        rating: child.val().rating
+                    });
+                });
 
-            if (data) {
-                setStallResult(Object.values(data));
-            }
+                setStallResult(stallResultAnswer)
+    
+            });
+        }
 
-        });
-    });
+    }, [db]);
+
 
     // Filtering by cuisines
 
+    useEffect(() => {
+        
+        if (cuisines != []) {
+            const cuisineSet = new Set(cuisines);
+            
+            if (name == '') {
+                const stallsMetadataReference = ref(db, 'stallsMetadata');                
 
-    return [dishresult, stallResult];
+                onValue(stallsMetadataReference, (snapshot) => {
+                    const stallResultAnswer = [];
+
+                    snapshot.forEach((child) => {
+                        if (cuisineSet.has(child.val().cuisine)) {
+                            stallResultAnswer.push({
+                                id: child.key,
+                                name: child.val().name,
+                                rating: child.val().rating,
+                                cuisine: child.val().cuisine
+                            });
+                        }
+                    });
+                });
+
+            } else {
+                const dishResultAnswer = [];
+                const stallResultAnswer = [];
+
+                dishResult.forEach((obj) => {
+                    if (cuisineSet.has(obj.cuisine)) {
+                        dishResultAnswer.push(obj);
+                    }
+                });
+
+                stallResult.forEach((obj) => {
+                    if (cuisineSet.has(obj.cuisine)) {
+                        stallResultAnswer.push(obj);
+                    }
+                });
+
+                setDishResult(dishResultAnswer);
+                setStallResult(stallResultAnswer);
+            }
+        }
+        
+    }, [db]);
+
+    return [dishResult, stallResult];
 
 }
 
--- src/Eat-at-NUS/Database/FilterDataTest.js
+++ src/Eat-at-NUS/Database/FilterDataTest.js
@@ -1,3 +1,4 @@
+// Importing the required stuff
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -5,24 +6,27 @@ import filterData from './FilterData';
 
 const FilterDataTest = () => {
 
-    const result = filterData('Pasta algio e olio', []);
+  const filterPress = () => {
+    const result = filterData('Pasta algio e olio', ["Western"]);
     console.log(result);
-
-    return (
-        <View style={styles.container}>
-            <Text>Filter Data Test</Text>
-        </View>
-    );
+  }
+  
+  return (
+  <View style={styles.container}>
+    <Text>Filter Data Test</Text>
+    <Text onPress={filterPress}>Press to check</Text>
+  </View>
+  );
 
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default FilterDataTest;
